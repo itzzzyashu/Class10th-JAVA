8463608bc0c30f812978cd8f88c7443a784d2548
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-09-01 19:13:24 +0530

    16th File

---
+++ Pattern.java
@@ -0,0 +1,17 @@
+// To display Floyd's Triangle
+public class Pattern
+{
+  public static void main(String args[])
+  {
+    int a,b,p=0;
+    for(a=1;a<=5;a++)
+    {
+      for(b=1;b<=a;b++)
+      {
+        p=p+1;
+        System.out.println(p+" ");
+      }
+      System.ou.println();
+    }
+  }
+}
