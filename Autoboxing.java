1261dbd2252e8e676f6bdf5330901dc1029be5a8
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-09-03 08:31:26 +0530

    19th File

--- Autoboxing.java
+++ Autoboxing.java
@@ -1,1 +1,12 @@
-hjgh
+// a sample perogram to illustrate Autoboxing and Unboxing
+class Autoboxing
+{
+  public static void main(String args[])
+  {
+    int x=25,y;
+    Integer val = new Integer(x); // Autoboxing
+    y=val; // Unboxing
+    System.out.println("Values after Autoboxing\t"+val);
+    System.out.println("Values after Unboxing\t"+y);
+  }
+}
