3dcd200d3e1002323804618231ed85d556b2502f
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-09-03 08:25:07 +0530

    18th File

---
+++ GCD.java
@@ -0,0 +1,20 @@
+// To calculate the GCD of two number
+import java.util.*;
+public class GCD
+{
+  public static void main(String args[])
+  {
+    Scanner in=new Scanner(System.in);
+    int a,b,i,p,gcd=0;
+    System.out.println("Enter Two Numbers");
+    a=in.nextInt();
+    b=in.nextInt();
+    p*b;
+    for(i=1;i<p;p++)
+    {
+      if(a%i==0 && b%i==0)
+        gcd=i;
+    }
+  }
+}
+    
